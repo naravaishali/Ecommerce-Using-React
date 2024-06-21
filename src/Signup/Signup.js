466738c6cd6f3f4bcb32ae256d1c6db5a34b5f7e
naravaishali <naravaishali@gmail.com>
2024-06-21 10:45:52 +0530

    first commit

---
+++ src/Signup/Signup.js
@@ -0,0 +1,65 @@
+import React , {useState}from 'react'
+import './Signup.css'
+import axios from 'axios'
+import  {useNavigate} from 'react-router-dom'
+
+export default function Signup() {
+    console.log(process.env.REACT_APP_BACKEND_URL)
+    const navigate=useNavigate()
+    const[name,setName]=useState('')
+    const[email,setEmail]=useState('')
+    const[password,setPassword]=useState('')
+    
+    const[mobile,setMobile]=useState('')
+    function handleSubmit(e){
+        e.preventDefault()
+        const newUser={name,password,email,mobile}
+        axios.post(`${process.env.REACT_APP_BACKEND_URL}/api/auth/register`,newUser)
+        .then((res) =>{
+            console.log(res)
+            if(res.status===201)
+                navigate('/login')
+        })
+        .catch((error)=>{
+            console.log(error)
+        })
+  
+    }
+  return (
+    <div className='signup-container'>
+        <h2>sign up</h2>
+        <form onSubmit={handleSubmit}>
+            <div className="form-group">
+                <label>Enter your Name : </label>
+                <input
+                   value={name}
+                   onChange={(e) => setName(e.target.value)}
+                required/>
+            </div>
+            <div className="form-group">
+                <label>Enter your Email : </label>
+                <input
+                   value={email}
+                   onChange={(e) => setEmail(e.target.value)}
+                required/>
+            </div>
+            <div className="form-group">
+                <label>Enter your Password: </label>
+                <input
+                   value={password}
+                   onChange={(e) => setPassword(e.target.value)}
+                required/>
+            </div>
+            <div className="form-group">
+                <label>Enter your Mobile: </label>
+                <input
+                   value={mobile}
+                   onChange={(e) => setMobile(e.target.value)}
+                required/>
+            </div>
+           <button>Signup</button>
+        </form>
+      
+    </div>
+  )
+}
